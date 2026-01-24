cb63932d88be1aea4d98f0e068e8b3173ec0b05f
Valmost3377 <Valmost3377@mail.ru>
2026-01-24 18:51:30 +0500

    slope beta

--- .venv/main.py
+++ .venv/main.py
@@ -1,15 +1,23 @@
+from email.iterators import typed_subpart_iterator
+
 import arcade
 from math import *
 import numpy as np
 import random
+
+from numpy.random.mtrand import Sequence
 
 FREQUENCY = 0.05
 n = 42
 grid1 = np.random.rand(n) * 2 - 1
 grid2 = np.random.rand(n // 3) * 2 - 1
 
-def gn(x, gr):
-    x0 = int(x)
+def gp(x, gr):
+    """--Get point--
+    Takes the Nth position (x) on a certain grid of nodes (gr).
+    Returns the Y value of the perlin noice (0.0 - 1.0).
+    """
+    x0 = int(x) # left node
     dx = x - x0
     s = 6 * dx ** 5 - 15 * dx ** 4 + 10 * dx ** 3
     g0 = gr[x0]
@@ -24,6 +32,33 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Damage Boost"
 GRAVITY = 12
+
+
+def calc_slope_collision(angle, xs, ys):
+    """
+    Calculate slope collision with vel: xs and ys.
+    Returns the updated xs and ys.
+    """
+    try:
+        angle_radians = radians(angle)
+        vector2 = np.array([
+            cos(angle_radians),
+            sin(angle_radians)
+        ])
+        vel = hypot(xs, ys)
+        vector1 = np.array((xs, ys))
+        norm1 = np.linalg.norm(vector1)
+        norm2 = np.linalg.norm(vector2)
+
+        normalized1 = vector1 / norm1
+        normalized2 = vector2 / norm2
+
+        cosine_sim = float(np.dot(normalized1, normalized2))
+
+        return np.round(vector2 * cosine_sim * vel, decimals=3)
+    except Exception as e:
+        print(e)
+        return np.array((xs, ys))
 
 
 class Arme(arcade.Sprite):
@@ -48,6 +83,7 @@ class Arme(arcade.Sprite):
         self.is_walking = False
         self.is_airborne = False
         self.lerp = 0.1
+        self.collisions = []
         ## Constants
         self.mass = 10
         self.ys = 0.0
@@ -60,6 +96,8 @@ class Arme(arcade.Sprite):
         self.speed_limit = 250.0
         self.boost = 50.0
         arcade.schedule(self.debug, 0.1)
+
+        self.ang = None
 
     def update(self, delta_time):
 
@@ -81,6 +119,11 @@ class Arme(arcade.Sprite):
 
     def debug(self, ar):
         print('Movement:', self.xs, self.ys)
+        print('Collisions:', self.collisions)
+        print('Ang:', pev.ang)
+        if pev.collisions:
+            print('Sent:', -pev.collisions[0].angle, pev.xs, pev.ys)
+            print('Received:', *calc_slope_collision(-pev.collisions[0].angle, pev.xs, pev.ys))
 
     def on_close(self):
         arcade.unschedule(self.debug)
@@ -97,19 +140,25 @@ class DamageBoost(arcade.Window):
         self.pev_list = arcade.SpriteList()
         self.pev_list.append(pev)
         self.keys_pressed = set()
-        self.walls = arcade.SpriteList()
+        self.walls = arcade.SpriteList(use_spatial_hash=True)
         self.setup()
 
         self.world_camera = arcade.camera.Camera2D()  # Камера для игрового мира
 
     def setup(self):
         print("setup")
-        for x in range(-SCREEN_WIDTH, SCREEN_WIDTH * 2, 1):
-            tile = arcade.Sprite("sprites/wall.png", scale=1/32)
+        for x in range(-SCREEN_WIDTH, SCREEN_WIDTH * 2, 512):
+            tile = arcade.Sprite("sprites/wall.png", scale=16)
             tile.center_x = x
-            tile.center_y = 32 + x / 4
+            tile.center_y = 32
             self.walls.append(tile)
 
+        for x in range(int(SCREEN_WIDTH * 0.8), SCREEN_WIDTH * 3, 128):
+            slope = arcade.Sprite("sprites/wall.png", scale=4)
+            slope.center_x = x
+            slope.center_y = 32 + x / 2
+            slope.angle = 15
+            self.walls.append(slope)
 
         self.engine = arcade.PhysicsEnginePlatformer(
             player_sprite=pev,
@@ -125,9 +174,14 @@ class DamageBoost(arcade.Window):
 
     def on_update(self, delta_time):
         pev.old_x, pev.old_y = pev.center_x, pev.center_y
+        pev.collisions = arcade.check_for_collision_with_list(pev, self.walls)
 
         pev.is_airborne = not self.engine.can_jump()
+        #if pev.collisions:
+        #    pev.is_airborne = abs(pev.collisions[0].angle) > 45
+
         if pev.is_airborne:  # ========= Airborne:
+            #pev.collisions = []
             pev.ys += -GRAVITY
             pev.ys += -pev.ys * abs(pev.ys) * pev.air_friction
 
@@ -137,6 +191,13 @@ class DamageBoost(arcade.Window):
                 pev.xs = 0.0
             else:
                 pev.xs *= pev.friction
+        try:
+            if pev.collisions and pev.ys != 0 and pev.xs != 0:
+                res = calc_slope_collision(-pev.collisions[0].angle, pev.xs, pev.ys)
+                pev.xs = res[0]
+                pev.ys = res[1]
+        except Exception as e:
+            print(e)
 
         # ======================== KEYS:
         if arcade.key.LEFT in self.keys_pressed and not pev.is_airborne:
@@ -167,7 +228,7 @@ class DamageBoost(arcade.Window):
             position,
             pev.lerp,  # Плавность следования камеры
         )
-        self.engine.update()
+        #self.engine.update()
 
         pev.xs = (pev.center_x - pev.old_x) / delta_time
         pev.ys = (pev.center_y - pev.old_y) / delta_time
